86de0040b2f26ef8e7029fa7ec12451cecf2c6e2
Alex Jeffery <apjeffery136@googlemail.com>
2016-11-24 11:43:11 +0200

    fixed the glider pattern

--- src/main/scala/model.scala
+++ src/main/scala/model.scala
@@ -77,7 +77,7 @@ object Grid {
 
   val gliderSouthEast: List [ ( Int, Int ) ] = List (
       ( -1 ,  0 )
-    , (  0 ,  0 )
+    , (  0 ,  1 )
     , (  1 , -1 )
     , (  1 ,  0 )
     , (  1 ,  1 ) )
